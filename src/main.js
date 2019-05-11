be5f368b955dacea7a129944fb31b33563e10776
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-11 19:31:22 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
+import router from './router';
+import store from './store';
+import ApiServices from './common/api.service'
 import 'vuetify/dist/vuetify.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(Vuetify, {
 	iconfont: 'fa'
-})
+});
+
+ApiServices.init();
 
 new Vue({
   router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
