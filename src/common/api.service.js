73d121f99477b207ec667ac62beb05c8b0b0cf9c
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-11 18:05:22 +0300

    Added api.services

---
+++ src/common/api.service.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import { API_URL } from "./config";
+
+const ApiServices = {
+	init() {
+		Vue.use(VueAxios, axios);
+		Vue.axios.defaults.baseURL = API_URL;
+	},
+
+	query(resource, params) {
+		return Vue.axios.get(resource, params).catch(error => {
+			throw new Error(`ApiService ${error}`);
+		})
+	},
+
+	get(resource, slug = "") {
+		return Vue.axios.get(`${resource}/${slug}`).catch(error => {
+			throw new Error(`ApiService ${error}`);
+		});
+	},
+
+	post(resource, params) {
+		return Vue.axios.post(`${resource}`, params);
+	}
+};
+
+export default ApiServices;
