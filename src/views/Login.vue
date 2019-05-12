cb948554d87a61ad3a5ae683f67e673cc8d70b12
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-13 00:27:11 +0300

    Added login vue template

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <h2># Login</h2>
+        <h5>... in develop</h5>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'Login',
+		props: {}
+	}
+</script>
+
+<style scoped>
+
+</style>
