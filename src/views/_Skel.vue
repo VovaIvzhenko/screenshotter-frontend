e13aa1ef2c67d1c2f665ea56c2816b9bfd232b62
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-13 15:26:57 +0300

    Added sample view

---
+++ src/views/_Skel.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="_skel">
+        <h1># _Skel</h1>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: '_Skel',
+		props: {}
+	}
+</script>
+
+<style scoped>
+._skel {
+    display: block;
+    height: 100%;
+    width: 100%;
+    background: red;
+}
+</style>
