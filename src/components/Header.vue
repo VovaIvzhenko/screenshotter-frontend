123a8372561d0ff4311a7fc5989508f68eb76012
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-13 13:39:13 +0300

    Updated Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,7 @@
             :fixed="true"
             :flat="true"
         >
+            <v-toolbar-side-icon v-if="isMobile" @click="isOpened = !isOpened"></v-toolbar-side-icon>
             <router-link to="/" tag="span" exact style="cursor: pointer;">
                 <v-toolbar-title>
                     <v-icon right dark>fas fa-camera</v-icon> ScreenShotter
@@ -14,8 +15,8 @@
             </router-link>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn to="screenshot" flat>ScreenShot</v-btn>
-                <v-btn to="pdf" flat>PDF</v-btn>
+                <v-btn to="/" flat>ScreenShot</v-btn>
+                <v-btn to="pdf" flat>PDF Generator</v-btn>
                 <v-btn to="login" flat>Login</v-btn>
             </v-toolbar-items>
         </v-toolbar>
@@ -23,10 +24,26 @@
 </template>
 
 <script>
-	export default {
-		name: 'Header',
-		props: {}
-	}
+import store from '@/store';
+import {mapGetters} from 'vuex'
+import {OPEN_DRAWER} from "../store/mutation.type";
+
+export default {
+    name: 'Header',
+    props: {},
+    computed: {
+        isOpened: {
+            get() {
+                return this.drawerOpened
+            },
+            set(value) {
+                store.commit(OPEN_DRAWER, value)
+            }
+        },
+        ...mapGetters(["drawerOpened", "isMobile"])
+    },
+    methods: {}
+}
 </script>
 
 <style scoped>
