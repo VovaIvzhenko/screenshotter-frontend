c4d189933f18807444ab65463cb1f23710bc3eec
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-13 23:02:41 +0300

    Added SnackBar component

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-snackbar
+            v-model="getSnackBar.active"
+            :color="getSnackBar.color"
+            :multi-line="getSnackBar.mode === 'multi-line'"
+            :timeout="getSnackBar.timeout"
+            :vertical="getSnackBar.mode === 'vertical'"
+    >
+        {{ getSnackBar.text }}
+        <v-btn
+                dark
+                flat
+                @click="getSnackBar.active = false"
+        >
+            Close
+        </v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+import store from '@/store';
+import {mapGetters} from 'vuex';
+import {SHOW_SNACK_BAR} from "../store/action.type";
+
+export default {
+    name: 'SnackBar',
+	computed: {
+		snackBarOpts: {
+			get() {
+				return this.getSnackBar
+			},
+			set(value) {
+				console.log('snackBarOpts -> SnackBar.vue -> ', value);
+				store.commit(SHOW_SNACK_BAR, value)
+			}
+		},
+		...mapGetters(["getSnackBar"])
+	},
+	methods: {}
+}
+</script>
+
+<style scoped>
+    ._skel {
+        display: block;
+        height: 100%;
+        width: 100%;
+        background: red;
+    }
+</style>
+
+
+
+
+
+
+
+
+
+
